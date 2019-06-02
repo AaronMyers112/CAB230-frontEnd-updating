4227c7ce983e0802678de3e68af5114b8f2cbba0
AaronMyers112 <39234018+AaronMyers112@users.noreply.github.com>
2019-06-02 13:42:36 +1000

    build of client side

---
+++ build/precache-manifest.b68b5294d2fe9d04102276e951537841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce974ceb4caa2b263a956dce70d387bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c8b9df039d47df2351",
+    "url": "/static/css/main.8e082147.chunk.css"
+  },
+  {
+    "revision": "36049b782dfd042d6ddb",
+    "url": "/static/js/2.8ac8a44d.chunk.js"
+  },
+  {
+    "revision": "87c8b9df039d47df2351",
+    "url": "/static/js/main.5e916b91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
